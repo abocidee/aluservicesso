f59a3c4fdf4f0960a6c941da58a13ce41c33d315
abocidee <770190990@qq.com>
2022-02-10 17:48:29 +0800

    dfg时

---
+++ src/main/java/com/alu/itoken/service/sso/config/MD5Util.java
@@ -0,0 +1,18 @@
+package com.alu.itoken.service.sso.config;
+
+
+import org.apache.commons.codec.binary.Hex;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class MD5Util {
+    public static String md5(String data) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return Hex.encodeHexString(md.digest(data.getBytes(StandardCharsets.UTF_8)));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
